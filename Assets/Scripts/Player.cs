36d45ef6037019ac98268c444f1ca31297721e6e
Silvia <silgadi@gmail.com>
2025-11-19 00:01:24 +0100

    wqqs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,11 +4,51 @@ public class Player : MonoBehaviour
 {
     private float x;
     private float y;
+    private int vida = 5;
+    private int dash = 0;
     void Update()
     {
         x = Input.GetAxisRaw("Horizontal");
         y = Input.GetAxisRaw("Vertical");
         
         this.gameObject.transform.position += new Vector3(x, y, 0).normalized * (Time.deltaTime * 4);
+    }
+
+    public void RestarVida()
+    {
+        vida--;
+        if (vida == 0)
+        {
+            Muerto();
+        }
+    }
+
+    public void SumarVida()
+    {
+        if (vida != 5)
+        {
+            vida++;
+        }
+    }
+
+    private void Muerto()
+    {
+        //Pantalla de Jugar Otra Vez
+        
+    }
+
+    private void Dash()
+    {
+        //multiplicar la velocidad del jugador durante 3 segundos y apagarla
+        //--dash;
+    }
+
+
+    private void OnTriggerEnter(Collider col)
+    {
+        //cuando colisione con un objeto de ataque enemigo RestarVida() a menos que use un parry que entonces gana un Dash
+        //si lo hace con algo guay SumarVida()
+        
+        //si lo hace con un asset movible lo puede tomar para tirarlo
     }
 }
